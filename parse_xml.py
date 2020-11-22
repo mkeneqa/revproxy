9ecb22701b6432767db98aa6c9a700345163953d
mkeneqa <mikeneqa@gmail.com>
2020-11-22 01:10:52 -0700

    xml parser class collect meta and save to sqlite

---
+++ parse_xml.py
@@ -0,0 +1,77 @@
+import xml.etree.ElementTree as ET
+import sqlite3
+from sqlite3 import Error
+import db_lite
+
+
+def term_in_file(search_term, full_path):
+    parts = full_path.split('/')
+    path_string = " ".join(parts)
+    if str(search_term).lower() in str(path_string).lower():
+        return 1
+    else:
+        return 0
+
+
+def get_file_name_ext_from_source(full_path: str):
+    parts = full_path.split('/')
+    name_ext = parts[len(parts) - 1]
+    return name_ext.split('.')
+
+
+def parse_asset_nodes(db: db_lite.DBLite, nodes: list):
+    rows = []
+    for node in nodes:
+        # [(UID, kind, file_ext, file_name, src, notes, within_fcp, proxy_file)]
+        row = (None, node.attrib['uid'], '', '', node.attrib['name'], '', node.tag, '', '')
+        rows.append(row)
+
+    db.insert_many_into(rows)
+
+
+def parse_media_nodes(db: db_lite.DBLite, nodes: list):
+    rows = []
+    for node in nodes:
+        # uid,kind,file_ext,file_name,src_path, notes
+        full_path = node.attrib['src']
+        name_ext = get_file_name_ext_from_source(full_path)
+        proxy_file = term_in_file('_proxy', full_path)
+        within_fcp = term_in_file('.fcpbundle', full_path)
+        # [(UID, kind, file_ext, file_name, src, notes, within_fcp, proxy_file)]
+        row = (None, node.attrib['sig'], node.attrib['kind'], name_ext[1], name_ext[0], full_path, node.tag, within_fcp, proxy_file)
+        rows.append(row)
+
+    db.insert_many_into(rows)
+
+
+if __name__ == '__main__':
+    db = db_lite.DBLite('test.db')
+    tree = ET.parse('Javen5yrsRuffCut_BASIC_EXPORT.fcpxml')
+    root = tree.getroot()
+    media_dict = {}
+    media_list = []
+    # print(root)
+    nodes = root.findall(".")
+    asset_nodes = root.findall("./resources/asset")
+    media_rep_nodes = root.findall("./resources/asset/media-rep")
+
+    parse_asset_nodes(db, asset_nodes)
+    parse_media_nodes(db, media_rep_nodes)
+
+    db.close_conn()
+
+    # for asset in root.findall('asset'):
+    #     for media in asset.findall('media-rep'):
+    #         media_kind = media.get('kind')
+    #         media_src = media.get('src')
+    #         media_uid = media.get('sig')
+    #
+    #         media_dict = {
+    #             "media_kind": media_kind,
+    #             "media_src": media_src,
+    #             "uid": "1234",
+    #             "ext": "mp4",
+    #             "is_proxy": True,
+    #
+    #
+    #         }
