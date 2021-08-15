3d4ba1286fb618261f718158abdccd725a20dab8
mkeneqa <mikeneqa@gmail.com>
2021-08-15 17:29:26 -0600

    cmd directory

---
+++ cmd/rproxy_cmd.py
@@ -0,0 +1,27 @@
+from cleo import Command
+from reverse_proxy_class import ReverseProxyWorkflow
+
+
+class RproxyCommand(Command):
+    """
+    Executes a Reverse Proxy Command
+
+    rproxy
+        {operation? : what operation?}
+        {--y|yell : If set, the task will yell in uppercase letters}
+    """
+
+    def handle(self):
+        ops = self.argument('operation')
+
+        if ops:
+            rproxy = ReverseProxyWorkflow()
+            rproxy.hello()
+            text = 'Operation {}'.format(ops)
+        else:
+            text = 'Hello'
+
+        if self.option('yell'):
+            text = text.upper()
+
+        self.line(text)
