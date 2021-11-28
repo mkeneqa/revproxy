0de8767dfbca301d1ead099738f81f189bb4d65c
mkeneqa <mikeneqa@gmail.com>
2021-11-28 16:26:31 -0700

    updated copy cmd

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 from cmd.greet_cmd import GreetCommand
 from cmd.rproxy_cmd import RproxyCommand
+from cmd.copy_cmd import CopyCommand
 from cleo import Application
 
 application = Application()
 application.add(GreetCommand())
 application.add(RproxyCommand())
+application.add(CopyCommand())
 
 if __name__ == '__main__':
     application.run()
